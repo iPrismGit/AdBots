97ccfe0fb9ce751d0622f89e9470205321a621c9
Omprakash <omprakash.mopuri@improve10x.com>
2026-01-16 10:27:46 +0530

    - Update `deploymentTargetSelector.xml` with a new device connection identifier and timestamp. - Add `graphicsLayer` import to `MainActivity.kt`.

--- app/src/main/java/com/iprism/adbots/MainActivity.kt
+++ app/src/main/java/com/iprism/adbots/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
